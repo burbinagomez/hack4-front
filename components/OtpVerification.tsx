3a55dfd969bfcd0f72bc3b299cf77157fa01923d
burbinagomez <51467293+burbinagomez@users.noreply.github.com>
2025-06-09 17:35:18 +0000

    Fix fecth request

--- components/OtpVerification.tsx
+++ components/OtpVerification.tsx
@@ -46,6 +46,7 @@ export default function OtpVerification({ email, domain, onBack, onComplete }: O
 
       if (data.user) {
         const headers = new Headers();
+        headers.append("Content-Type", "application/json");
         headers.append("x-api-key", process.env.NEXT_PUBLIC_API_KEY);
         fetch(process.env.NEXT_PUBLIC_API+"/report",{
           method: "POST",
